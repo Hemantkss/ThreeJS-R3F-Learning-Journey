c19ac42fe48a0129952ddc1cb0fcc7104033f057
Hemant Sabale <hemantsabale61@gmail.com>
2025-02-16 23:05:00 +0530

    Add files via upload

---
+++ 23 Shaders/src/shaders/test/vertex.glsl
@@ -0,0 +1,26 @@
+precision mediump float;
+
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;      // Related to Camera
+uniform mat4 modelMatrix; // Related to Mesh
+
+attribute vec3 position;
+
+// Custom attribute
+attribute float aRandom;
+varying float vRandom;
+
+void main() {
+
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    modelPosition.z += aRandom * 0.1;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+
+    vRandom = aRandom;
+
+}
