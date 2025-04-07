258b789ba0c8d2b81d29d6c70d39b9e33b1eafd3
Hemant Sabale <hemantsabale61@gmail.com>
2025-04-07 12:53:25 +0530

    Add files via upload

---
+++ 45 Portal Scene/src/shaders/fireflies/fragment.glsl
@@ -0,0 +1,8 @@
+void main()
+{
+    // Pattern for the firefly
+    float distanceToCenter = distance(gl_PointCoord, vec2(0.5));
+    float strength =  0.05 / distanceToCenter - 0.1;
+
+    gl_FragColor = vec4(1.0, 1.0, 1.0, strength);
+}
